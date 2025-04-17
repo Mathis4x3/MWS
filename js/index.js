38b435221df9f076a6d53511fbdb238285635afc
Ourliac Mathis <mathis OURLIAC>
2025-04-17 19:08:13 +0200

    mastermind page

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const c1 = document.querySelector('#c1')
+
+const carousel1 = new bootstrap.Carousel(c1, {
+    interval: 2000,
+    touch: false
+})
+
+const c2 = document.querySelector('#c2')
+
+const carousel2 = new bootstrap.Carousel(c2, {
+    interval: 2000,
+    touch: false
+})
+
+const c3 = document.querySelector('#c3')
+
+const carousel3 = new bootstrap.Carousel(c3, {
+    interval: 2000,
+    touch: false
+})
+
+const c4 = document.querySelector('#c4')
+
+const carousel4 = new bootstrap.Carousel(c4, {
+    interval: 2000,
+    touch: false
+})
+
+const halo = document.querySelector('.halo');
+
+document.addEventListener('mousemove', (event) => {
+    if(window.innerWidth > 992) {
+        halo.style.top = `${event.clientY}px`;
+        halo.style.left = `${event.clientX}px`;
+    }
+});
