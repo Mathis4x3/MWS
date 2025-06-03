14c9d3e6ea5ba74332d4aaaf08f4f4f248829787
Mathis Ourliac <mathis.ourliac@etu.umontpellier.fr>
2025-06-03 22:52:51 +0200

    site

--- js/index.js
+++ js/index.js
@@ -23,3 +23,15 @@ document.addEventListener('mousemove', (event) => {
         halo.style.left = `${event.clientX}px`;
     }
 });
+
+let bulles = document.getElementsByClassName("bulle");
+for (let i = 0; i < bulles.length; i++) {
+    setInterval(() => {
+        let transform = bulles[i].style.transform;
+        if(transform === "rotate(10deg)") {
+            bulles[i].style.transform = "rotate(-10deg)";
+        }else{
+            bulles[i].style.transform = "rotate(10deg)";
+        }
+    }, 1000);
+}
